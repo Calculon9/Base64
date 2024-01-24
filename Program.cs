281bcb3a0cda04ea29431417503b24f2a6efa615
Callum <callum@equ.com.au>
2024-01-24 14:55:25 +0800

    added padding function

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿
+using System.Collections;
+using System.Reflection;
+
 var str = "hi";
 
 //Convert to ASCII Codes
@@ -18,14 +21,39 @@ var bin_code = new Int32[str.Length * 8];
 Console.WriteLine("Convert to binary");
 for (int i = 0; i < ascii_code.Length; i++)
 {
-    var asciiChar = ascii_code[i];
-    var binStr = Convert.ToString(asciiChar, 2);
-    for (int j = i + 1; j % 8 != 0; j++)
+    var asciiNum = ascii_code[i];
+    var binArr = PaddTo8Bits(asciiNum);
+    
+    //Add to combined binary array
+    for (int j = 0; j < binArr.Length; j++)
     {
-        bin_code[j] = ;
+        bin_code[(i * 8) + j] = binArr[j];
     };
-    bin_code[i] = str[i];
+}
 
-    //Console.Write(ascii_code[i]);
-    Console.Write(binStr + ",");
+Console.Write(string.Join(" ", bin_code));
+
+
+
+
+
+
+static int[] PaddTo8Bits(int asciiNum)
+{
+    var asciiBinArr = Convert.ToString(asciiNum, 2).ToCharArray();
+    var paddingBits = 8 % asciiBinArr.Length;
+
+    if(asciiBinArr.Length > 8) { throw new Exception("Error: The provided ASCII code's binary contains >8 bits. A conversion from the ASCII character to its numerical value has likely failed. Cannot continue."); }
+
+    //Add padding
+    var binArr = new int[8];
+
+    //Add remaining bits - or all if no padding was needed
+    for (int j = paddingBits; j < binArr.Length; j++)
+    {
+        //Console.WriteLine(asciiBinArr[j]);
+        binArr[j] = Int32.Parse(asciiBinArr[j - paddingBits].ToString());
+    }
+
+    return binArr;
 }
